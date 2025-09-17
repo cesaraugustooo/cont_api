b33d1a3cb0f96ecefe1911ec766d6be10bc20cc2
Cesar Augusto <cesaraugusto23121989@gmail.com>
2025-09-17 15:48:33 -0300

    commit add Categoria Model

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -4,7 +4,36 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Categoria
+ *
+ * @property $id_categoria
+ * @property $nome_categoria
+ *
+ * @property Turma[] $turmas
+ * @property Turma[] $turmas
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
 class Categoria extends Model
 {
-    //
+    public $primaryKey = 'id_categoria';
+    public $timestamps = false;
+    protected $perPage = 20;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [ 'nome_categoria'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function turmas()
+    {
+        return $this->hasMany(\App\Models\Turma::class, 'id_categoria', 'categorias_id_categoria');
+    }
+    
 }
