c93a578ac6ab01317ccfead8a4399686ac72ffa1
Cesar Augusto <cesaraugusto23121989@gmail.com>
2025-09-17 15:47:06 -0300

    commit add Cateforia Controller

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -4,62 +4,57 @@ namespace App\Http\Controllers;
 
 use App\Models\Categoria;
 use Illuminate\Http\Request;
+use App\Http\Requests\CategoriaRequest;
+use Illuminate\Http\Response;
+use Illuminate\Http\JsonResponse;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\CategoriaResource;
+use App\Services\CategoriaService;
 
 class CategoriaController extends Controller
 {
+    public $service;
+
+    public function __construct() {
+        $this->service = new CategoriaService();
+    }
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        return $this->service->index($request);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CategoriaRequest $request): JsonResponse
     {
-        //
+        return $this->service->store($request);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Categoria $categoria)
+    public function show(Categoria $categoria): JsonResponse
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Categoria $categoria)
-    {
-        //
+        return $this->service->show($categoria);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Categoria $categoria)
+    public function update(CategoriaRequest $request,Categoria $categoria): JsonResponse
     {
-        //
+        return $this->service->update($request,$categoria);
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Delete the specified resource.
      */
-    public function destroy(Categoria $categoria)
+    public function destroy(Categoria $categoria): Response
     {
-        //
+        return $this->service->destroy($categoria);
     }
 }
